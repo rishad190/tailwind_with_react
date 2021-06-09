0268ccb0c8f6894c18c0ccb70e65c5a3e32c1cb0
Md. Rishad Khan <rishadkhan190@gmail.com>
2021-06-09 21:04:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Banner from "./Components/Home/Banner/Banner";
+import Header from "./Components/Home/Header/Header";
+import MidCom from "./Components/Home/MidCom/MidCom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <MidCom />
+      <Banner />
     </div>
   );
 }
